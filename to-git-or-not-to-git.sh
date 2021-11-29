9442144d45f7e0f2cb2af4b36941b39ebc9b7bc0
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-11-29 14:39:16 +0000

    My very second commit

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,2 +1,2 @@
-curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{u
-echo {"data":{"user":[{"id":1907}]}}
+curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"Cassidy.Hall94\"}}){id}}"}'|jq .id
+
