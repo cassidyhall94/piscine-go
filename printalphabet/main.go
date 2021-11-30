e6f05c133efa4386596fa15154bdc69823c2d9f4
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-11-30 15:29:24 +0000

    go first

---
+++ printalphabet/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	var alphabet string = "abcdefghijklmnopqrstuvwxyz"
+	for i := 0; i <= 25; i++ {
+		z01.PrintRune(rune(alphabet[i]))
+	}
+
+	z01.PrintRune('\n')
+}
