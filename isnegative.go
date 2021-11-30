dde1ea1c87824f159e488fa02111f32763cf1316
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-11-30 17:44:31 +0000

    negative.go

--- isnegative.go
+++ isnegative.go
@@ -1,11 +1,11 @@
-package isnegative
+package IsNegative
 
 import (
 	"github.com/01-edu/z01"
 )
 
 func IsNegative(nb int) {
-	if nb < 0 {
+	if nb <= 0 {
 		z01.PrintRune('T')
 		z01.PrintRune('\n')
 	} else {
