ab11befde4462974839bea113d8a61fc0821dd92
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-11-30 16:55:33 +0000

    isnegative.go

---
+++ isnegative.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func isNegative(num int) {
+	if num >= 0 {
+		fmt.println("T")
+	} else {
+		fmt.Println("F")
+	}
+	}
+
+	z01.PrintRune('\n')
+}
