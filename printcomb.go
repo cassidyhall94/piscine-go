081f35243d0e8900633487c29c013643e74d9401
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-01 11:34:33 +0000

    printcomb

---
+++ printcomb.go
@@ -0,0 +1,20 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	aRune := "0123456789"
+	for a := 0; a <= 9; a++ {
+	for b := 0; b <= 9; b++ {
+	for c := 0; c <= 9; c++ {
+	 {
+		
+	}
+	if nb < 0 && nb > 0 {
+		z01.PrintRune('T')
+	} else if {
+		z01.PrintRune('F')
+	} else {
+		z01.PrintRune('F')
+	}
+}
