5f02e521ec4476b87593bc2d2aab6602b4e7e643
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-01 11:46:11 +0000

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -3,18 +3,22 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	aRune := "0123456789"
+	comb := "0123456789"
 	for a := 0; a <= 9; a++ {
-	for b := 0; b <= 9; b++ {
-	for c := 0; c <= 9; c++ {
-	 {
-		
-	}
-	if nb < 0 && nb > 0 {
-		z01.PrintRune('T')
-	} else if {
-		z01.PrintRune('F')
-	} else {
-		z01.PrintRune('F')
+		for b := 0; b <= 9; b++ {
+			for c := 0; c <= 9; c++ {
+				if a < b && b < c {
+					z01.PrintRune(rune(comb[a]))
+					z01.PrintRune(rune(comb[b]))
+					z01.PrintRune(rune(comb[c]))
+					if a == 7 && c == 9 {
+						z01.PrintRune('\n')
+					} else {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
 	}
 }
