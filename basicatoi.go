a9d1c1733a7252fead690b9135824ac3d2540571
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-02 13:54:26 +0000

    basicatoi

---
+++ basicatoi.go
@@ -0,0 +1,12 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	runes := []rune(s)
+
+	var result []int
+
+	for i := 0; i < len(runes); i++ {
+		result = append(result, int(runes[i]))
+	}
+	return result
+}
