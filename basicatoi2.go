5611722f18cece362cf88050c13a775b09ad4c2d
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-02 15:00:24 +0000

    basicatoi2

--- basicatoi2.go
+++ basicatoi2.go
@@ -1,18 +1,14 @@
 package piscine
 
-func BasicAtoi(s string) int {
+func BasicAtoi2(s string) int {
 	result := 0
 	for _, num := range s {
 		conv := int(num) - 48
-		result = (result * 10) + conv
-	}
-	if num[i] < 48 || num[i] > 57 {
 
-		return 0
-	} else {
-		result *= 10
-		result += int(num[i]) - 48
+		if num[i] < 48 || num[i] > 57 {
+		} else {
+			result = (result * 10) + conv
+		}
 	}
-}
 	return result
 }
