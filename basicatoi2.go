fc9ff8cfd9bdb14e45d006e812e2f9d671e8fb9f
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-02 15:08:00 +0000

    basicatoi2

--- basicatoi2.go
+++ basicatoi2.go
@@ -2,11 +2,10 @@ package piscine
 
 func BasicAtoi2(s string) int {
 	result := 0
-	var num rune := []rune(s)
 	for _, num := range s {
 		conv := int(num) - 48
 
-		if num[i] < '0' || num[i] > '9' {
+		if num <= 0 || num >= 0 {
 		} else {
 			result = (result * 10) + conv
 		}
