74d325f40f45e8d3fe89195fbcd4765b87252009
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-02 15:30:23 +0000

    basicatoi2

--- basicatoi2.go
+++ basicatoi2.go
@@ -2,13 +2,22 @@ package piscine
 
 func BasicAtoi2(s string) int {
 	result := 0
-	for _, num := range s {
-		conv := int(num) - 48
-
-		if num <= 0 || num >= 0 {
+	b := 0
+	check := true
+	a := []rune(s)
+	for _, num := range a {
+		if byte(num) >= 48 && byte(num) <= 57 {
+			for i := '0'; i < num; i++ {
+				b++
+			}
+			result = (result * 10) + b
+			b = 0
 		} else {
-			result = (result * 10) + conv
+			check = false
+		}
+		if check {
+			return result
+		} else {
+			return 0
 		}
 	}
-	return result
-}
