587aaf3223351ebec59eaaff266dc0f4b99ba660
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-06 11:40:17 +0000

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,10 +1,15 @@
 package piscine
 
+import "github.com/01-edu/z01"
+
 func IterativeFactorial(nb int) int {
-	result := 14
+	result := 0
 
 	for i := 1; i < nb+1; i++ {
 		result = result + i
+	}
+	if nb < 0 || nb > 24 {
+		z01.PrintRune(48)
 	}
 	return result
 }
