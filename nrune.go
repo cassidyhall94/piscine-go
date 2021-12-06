2a92ab6e09bbbaaf966975a84749eed3af0c3594
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-06 21:43:19 +0000

    nrune

--- nrune.go
+++ nrune.go
@@ -4,5 +4,5 @@ func NRune(s string, n int) rune {
 	if n > (len([]rune(s))) { // n is greater than the rune array [] length/ not possible because you cant return the rune if n is greater
 		return rune(0) // first rune
 	}
-	return []rune(s)[n-1] // nth rune, if n is valid number (less than or equal to length of array)
+	return []rune(s)[n] // nth rune, if n is valid number (less than or equal to length of array)
 }
