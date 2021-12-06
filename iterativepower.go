fe3e3d73505b57667061438fe2ab8ff32b770648
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-06 16:41:03 +0000

    iterativepower

--- iterativepower.go
+++ iterativepower.go
@@ -5,7 +5,7 @@ func IterativePower(nb int, power int) int {
 	if power < 0 {
 		return 0
 	}
-	if nb == 0 || power == 0 {
+	if nb == 0 && power == 0 {
 		return 1
 	} else {
 		for i := 0; i < power-1; i++ {
