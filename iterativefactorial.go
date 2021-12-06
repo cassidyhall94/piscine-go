9bfad7c115496cdf76dd2d233db80cfa72c3af46
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-06 12:42:33 +0000

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,14 +2,14 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 0
+	if result == 0 {
+		return 1
+	}
 	for i := nb; i > nb-1; i-- {
 		result = i * (i - 1) * (i - 2)
 	}
 	if result <= 0 || result > 24 {
 		return 0
-	}
-	if result == 0 {
-		return 1
 	}
 	return result
 }
