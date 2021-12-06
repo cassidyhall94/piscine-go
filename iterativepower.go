27272881765b9c0a14c391744383e893438a371a
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-06 15:31:22 +0000

    iterativepower

--- iterativepower.go
+++ iterativepower.go
@@ -1,11 +1,8 @@
 package piscine
 
 func IterativePower(nb int, power int) int {
-	if nb < 0 {
+	if power < 0 {
 		return 0
-	}
-	if nb == 0 {
-		return 1
 	} else {
 		power := nb
 		for i := 0; i < nb+1; i++ {
