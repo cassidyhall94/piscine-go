3477c39e74b74b652989b544e711c2dde8fd553b
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-06 21:55:22 +0000

    nrune

--- nrune.go
+++ nrune.go
@@ -3,7 +3,6 @@ package piscine
 func NRune(s string, n int) rune {
 	if n > (len([]rune(s))) { // n is greater than the rune array [] length/ not possible because you cant return the rune if n is greater
 		return rune(0)
-	} else {
-		return []rune(s)[n-1] // nth rune, if n is valid number (less than or equal to length of array)
 	}
+	return []rune(s)[n-1] // nth rune, if n is valid number (less than or equal to length of array)
 }
