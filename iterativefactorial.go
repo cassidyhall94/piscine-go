d7737fde5ef7293b84c9cedf1be369667da47380
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-06 12:33:39 +0000

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,9 +4,9 @@ func IterativeFactorial(nb int) int {
 	result := 0
 	for i := nb; i > nb-1; i-- {
 		result = i * (i - 1) * (i - 2)
-		if result < 0 || result > 24 {
-			return 0
-		}
+	}
+	if result < 0 || result > 24 {
+		return 0
 	}
 	return result
 }
