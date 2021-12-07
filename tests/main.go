b0c59d403bac940b5792cf17406b4909342cb2e7
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-07 15:42:51 +0000

    concat

--- tests/main.go
+++ tests/main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.Index("Hello!", "l"))
-	fmt.Println(piscine.Index("Salut!", "alu"))
-	fmt.Println(piscine.Index("ll_b}5c7<gt4n", "g0R;'($\\=U9/~"))
+	fmt.Println(piscine.Concat("Hello!", " How are you?"))
+
 }
