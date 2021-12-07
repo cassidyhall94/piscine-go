c05fc2930a491847c8106a022b9b17d44b5932ec
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-07 15:39:19 +0000

    index

--- tests/main.go
+++ tests/main.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	fmt.Println(piscine.Index("Hello!", "l"))
 	fmt.Println(piscine.Index("Salut!", "alu"))
-	fmt.Println(piscine.Index("Ola!", "hOl"))
+	fmt.Println(piscine.Index("ll_b}5c7<gt4n", "g0R;'($\\=U9/~"))
 }
