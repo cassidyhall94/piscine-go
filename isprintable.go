98a0679cf97993cd8507918ff2aa1ba3d5fd3ef6
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-07 17:09:42 +0000

    isprintable

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package piscine
 func IsPrintable(s string) bool {
 	counter := 0
 	for _, stringrune := range []rune(s) {
-		if (stringrune >= rune(33)) && (stringrune <= rune(126)) {
+		if (stringrune >= rune(32)) && (stringrune <= rune(126)) {
 			counter++
 		} else {
 			return false
