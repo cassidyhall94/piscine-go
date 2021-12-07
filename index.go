d2e4e640389e29cade6feb4be69be435c0e986d1
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-07 13:27:23 +0000

    index

--- index.go
+++ index.go
@@ -1,6 +1,10 @@
 package piscine
 
 func Index(s string, toFind string) int {
+	// to check if tofind has nothing in the string
+	if toFind == "" || s == "" {
+		return 0
+	}
 	// looping through tofind and s strings to compare their letters, and print their index if they are equal.
 	for _, toFindrune := range []rune(toFind) {
 		for index, srune := range []rune(s) {
