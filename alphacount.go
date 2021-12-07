a7cbb03c0d37c4bf978958527a143fefe2b0dc5b
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-07 11:19:49 +0000

    alphacount

--- alphacount.go
+++ alphacount.go
@@ -2,8 +2,8 @@ package piscine
 
 func AlphaCount(s string) int {
 	counter := 0
-	for _, stringrune := range []rune(s) {
-		if ((stringrune >= rune(65)) && (stringrune <= rune(90))) || ((stringrune >= rune(97)) && (stringrune <= rune(122))) {
+	for _, stringrune := range []rune(s) { // looping through s string by slice
+		if ((stringrune >= rune(65)) && (stringrune <= rune(90))) || ((stringrune >= rune(97)) && (stringrune <= rune(122))) { // checks to see if any of the runes aren't part of the alphabet using ascii table numbers for upper and lower case
 			counter++ // means counter = counter + 1
 		}
 	}
