f51e4f95795441a52089000135ee382791dd0348
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-08 16:26:04 +0000

    printprogramname

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -7,9 +7,11 @@ import (
 )
 
 func main() {
-	argue := os.Args
-	for _, prog := range argue[0] {
-		z01.PrintRune(prog)
+	argue := os.Args[0]
+	for i, prog := range argue {
+		if i > 1 {
+			z01.PrintRune(rune(prog))
+		}
 	}
 	z01.PrintRune('\n')
 }
