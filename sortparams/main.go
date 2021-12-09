f5ddd6f480d398b36f53b4dd6e6586af39c46237
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-09 19:34:09 +0000

    sortparams

--- sortparams/main.go
+++ sortparams/main.go
@@ -8,21 +8,26 @@ import (
 
 func main() {
 	// slicerune == num
-	var slicerune []rune
+	var slicerune [][]rune
 	for _, arg := range os.Args[1:] { // to loop over the command one by one
-		r := rune(arg[0])
-		slicerune = append(slicerune, r)
+		rs := []rune{}
+		for _, argrune := range arg {
+			rs = append(rs, argrune)
+		}
+		slicerune = append(slicerune, rs)
 	}
 	sortascii := len(slicerune)
 	for i := 0; i < sortascii; i++ {
 		for j := 0; j < (sortascii - 1 - i); j++ {
-			if slicerune[j] > slicerune[j+1] {
+			if slicerune[j][0] > slicerune[j+1][0] {
 				slicerune[j], slicerune[j+1] = slicerune[j+1], slicerune[j]
 			}
 		}
 	}
-	for i := range slicerune {
-		z01.PrintRune(rune(slicerune[i]))
+	for _, sr := range slicerune {
+		for _, j := range sr {
+			z01.PrintRune(j)
+		}
 		z01.PrintRune('\n')
 	}
 }
