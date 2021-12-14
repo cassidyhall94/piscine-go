d6326ed1e9dfb4f86364ca3ac6265977d17772a8
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-14 12:57:47 +0000

    point

---
+++ point/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+
+	points := &point{}
+
+	setPoint(points)
+
+	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+}
