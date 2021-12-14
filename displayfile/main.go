43cea44f18a641902279735f84e4675d2dd1e3ef
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-14 19:05:43 +0000

    displayfile

--- displayfile/main.go
+++ displayfile/main.go
@@ -6,13 +6,13 @@ import (
 )
 
 func main() {
-	fname := "File name missing"
-	file, err := os.Open(fname)
+	file, err := os.Open("quest8.txt")
 	if err != nil {
-		fmt.Print("File name missing\n")
+		fmt.Printf("File name missing\n")
 	} else {
-		arr := make([]byte, 6)
+		arr := make([]byte, 20)
 		file.Read(arr)
+		fmt.Println(string(arr))
 		file.Close()
 	}
 }
