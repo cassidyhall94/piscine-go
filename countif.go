af662f98161fd4576971f2b00b3c6349dfec1d8b
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-15 14:04:36 +0000

    countif

--- countif.go
+++ countif.go
@@ -4,8 +4,7 @@ func CountIf(f func(string) bool, tab []string) int {
 	var result int
 	counter := 0
 	for _, slice := range tab {
-		f(slice)
-		if slice >= "0" && slice <= "9" {
+		if f(slice) {
 			counter++
 		}
 	}
