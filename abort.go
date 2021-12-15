2d46e5c258b2273637418a04dbf323d390631f25
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-15 20:31:44 +0000

    abort

---
+++ abort.go
@@ -0,0 +1,15 @@
+package piscine
+
+func Abort(a, b, c, d, e int) int {
+
+	median := []int{a, b, c, d, e}
+	sortinterger := len(median)
+	for i := 0; i < sortinterger; i++ {
+		for j := 0; j < (sortinterger - 1 - i); j++ {
+			if median[j] > median[j+1] {
+				median[j], median[j+1] = median[j+1], median[j]
+			}
+		}
+	}
+	return median[2]
+}
--- tests/main.go
+++ tests/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"piscine"
-
-	"github.com/01-edu/z01"
 )
 
 func main() {
-	result := piscine.Rot14("MXzO23NF Yxd7")
-
-	for _, r := range result {
-		z01.PrintRune(r)
-	}
-	z01.PrintRune('\n')
+	middle := piscine.Abort(2, 3, 8, 5, 7)
+	fmt.Println(middle)
 }
