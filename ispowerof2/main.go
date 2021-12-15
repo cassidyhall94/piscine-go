9a5d3e98ae4662b52d3c687d0b28ecb2219e834b
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2021-12-15 23:37:11 +0000

    ispowerof2

--- ispowerof2/main.go
+++ ispowerof2/main.go
@@ -10,29 +10,30 @@ import (
 func main() {
 	if len(os.Args) > 2 {
 		return
-	}
-	n, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		for _, r := range "false\n" {
-			z01.PrintRune(r)
-		}
-		return
-	}
-
-	if n == 0 {
-		for _, r := range "false\n" {
-			z01.PrintRune(r)
-		}
-		return
-	}
-
-	if isPower(n) == 0 {
-		for _, r := range "true\n" {
-			z01.PrintRune(r)
-		}
 	} else {
-		for _, r := range "false\n" {
-			z01.PrintRune(r)
+		n, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			for _, r := range "false\n" {
+				z01.PrintRune(r)
+			}
+			return
+		}
+
+		if n == 0 {
+			for _, r := range "false\n" {
+				z01.PrintRune(r)
+			}
+			return
+		}
+
+		if isPower(n) == 0 {
+			for _, r := range "true\n" {
+				z01.PrintRune(r)
+			}
+		} else {
+			for _, r := range "false\n" {
+				z01.PrintRune(r)
+			}
 		}
 	}
 }
