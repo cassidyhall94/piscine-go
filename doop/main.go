504f7c05c06ccb4e5306162c3894e1df9b87640c
cassidy.hall94 <cassidy.hall94@gmail.com>
2021-12-16 16:45:39 +0000

    doop

--- doop/main.go
+++ doop/main.go
@@ -5,50 +5,70 @@ import (
 	"strconv"
 )
 
-func isSigns(str string, arr []string) bool {
-	for _, v := range arr {
-		if str == v {
-			return true
+func main() {
+	args := os.Args[1:]
+	if len(args) == 3 {
+		works := true
+		_, err := strconv.Atoi(args[0])
+		_, err2 := strconv.Atoi(args[2])
+		if err != nil || err2 != nil {
+			works = false
+		}
+		if works {
+			os.Stdout.WriteString(Maths(args))
 		}
 	}
-	return false
 }
 
-func main() {
-	signs := []string{"+", "*", "-", "/", "%"}
-	args := os.Args[1:]
-	if len(args) != 3 {
-	} else {
-		if isSigns(args[1], signs) {
-			nb1, err := strconv.Atoi(args[0])
-			nb2, err2 := strconv.Atoi(args[2])
-			if err == nil && err2 == nil {
-				switch args[1] {
-				case "+":
-					os.Stderr.WriteString("nb1 + nb2")
-				case "-":
-					os.Stderr.WriteString("nb1 - nb2")
-				case "/":
-					if nb2 == 0 {
-						os.Stderr.WriteString("No division by 0")
-					} else {
-						os.Stderr.WriteString("nb1 / nb2")
-					}
-				case "%":
-					if nb2 == 0 {
-						os.Stderr.WriteString("No Modulo by 0")
-					} else {
-						os.Stderr.WriteString("nb1 % nb2")
-					}
-				case "*":
-					os.Stderr.WriteString("nb1 * nb2")
-
-				}
-			} else {
-				os.Stderr.WriteString("1")
-			}
-		} else {
-			os.Stderr.WriteString("0")
+func Maths(s []string) string {
+	nb1, _ := strconv.Atoi(s[0])
+	nb2, _ := strconv.Atoi(s[2])
+	var result int
+	var output string
+	switch s[1] {
+	case "+":
+		if nb1 == 9223372036854775807 || nb2 == 9223372036854775807 {
+			return ""
 		}
+		result = nb1 + nb2
+		output = changetoString(result)
+		os.Stdout.WriteString("nb1 + nb2")
+	case "-":
+		if nb1 == 9223372036854775807 || nb2 == 9223372036854775807 {
+			return ""
+		}
+		result = nb1 - nb2
+		output = changetoString(result)
+		os.Stdout.WriteString("nb1 - nb2")
+	case "/":
+		if nb2 == 0 || nb1 == 0 {
+			result = nb1 / nb2
+			output = changetoString(result)
+			os.Stdout.WriteString("No division by 0")
+		} else {
+			os.Stdout.WriteString("nb1 / nb2")
+		}
+	case "%":
+		if nb2 == 0 || nb1 == 0 {
+			result = nb1 % nb2
+			output = changetoString(result)
+			os.Stdout.WriteString("No Modulo by 0")
+		} else {
+			os.Stdout.WriteString("nb1 % nb2")
+		}
+	case "*":
+		if nb1 == 9223372036854775807 || nb2 == 9223372036854775807 {
+			return ""
+		}
+		result = nb1 * nb2
+		output = changetoString(result)
+		os.Stdout.WriteString("nb1 * nb2")
 	}
+	return output
+}
+
+func changetoString(nbr int) string {
+	var output string
+	output = strconv.Itoa(nbr)
+	return output + "\n"
 }
