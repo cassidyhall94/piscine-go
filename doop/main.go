7c7d090f6591ab75f3b7ded0eff58766c5f9d696
cassidy.hall94 <cassidy.hall94@gmail.com>
2021-12-16 15:57:58 +0000

    doop

--- doop/main.go
+++ doop/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"strconv"
 )
 
-func isSigne(str string, arr []string) bool {
+func isSigns(str string, arr []string) bool {
 	for _, v := range arr {
 		if str == v {
 			return true
@@ -16,40 +15,40 @@ func isSigne(str string, arr []string) bool {
 }
 
 func main() {
-	signe := []string{"+", "*", "-", "/", "%"}
+	signs := []string{"+", "*", "-", "/", "%"}
 	args := os.Args[1:]
 	if len(args) != 3 {
 	} else {
-		if isSigne(args[1], signe) {
+		if isSigns(args[1], signs) {
 			nb1, err := strconv.Atoi(args[0])
 			nb2, err2 := strconv.Atoi(args[2])
 			if err == nil && err2 == nil {
 				switch args[1] {
 				case "+":
-					fmt.Println(nb1 + nb2)
+					os.Stderr.WriteString("nb1 + nb2")
 				case "-":
-					fmt.Println(nb1 - nb2)
+					os.Stderr.WriteString("nb1 - nb2")
 				case "/":
 					if nb2 == 0 {
-						fmt.Println("No division by 0")
+						os.Stderr.WriteString("No division by 0")
 					} else {
-						fmt.Println(nb1 / nb2)
+						os.Stderr.WriteString("nb1 / nb2")
 					}
 				case "%":
 					if nb2 == 0 {
-						fmt.Println("No Modulo by 0")
+						os.Stderr.WriteString("No Modulo by 0")
 					} else {
-						fmt.Println(nb1 % nb2)
+						os.Stderr.WriteString("nb1 % nb2")
 					}
 				case "*":
-					fmt.Println(nb1 * nb2)
+					os.Stderr.WriteString("nb1 * nb2")
 
 				}
 			} else {
-				fmt.Println("1")
+				os.Stderr.WriteString("1")
 			}
 		} else {
-			fmt.Println("0")
+			os.Stderr.WriteString("0")
 		}
 	}
 }
